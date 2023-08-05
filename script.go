4040ce618702bf7ecb49dfd67f5347fc6d9d352c
Kate <138806480+bahtiarm13@users.noreply.github.com>
2023-08-05 16:00:36 +0300

    Update script.go

--- script.go
+++ script.go
@@ -45,3 +45,5 @@ func Args() *Pipe {
 }
 
 // Do creates a pipe that makes the HTTP request req and produces the response.
+// See [Pipe.Do] for how the HTTP response status is interpreted.
+func Do(req *http.Request) *Pipe {
